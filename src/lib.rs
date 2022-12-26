abaf6c5b9a60d935e34a4a90e26ddf16c434cbec
probro27 <prabhavkhera@hotmail.com>
2022-12-25 23:17:04 -0500

    Chat gpt errors, figuring out open ai

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::env::args;
+use std::{env::args, process::exit};
 use checked_command::{CheckedCommand, Error};
 
 #[derive(Debug, Clone)]
@@ -13,7 +13,11 @@ pub struct CommandResult {
 }
 
 pub fn get_command() -> CommandExecute {
-    let mut args = args().collect::<Vec<String>>()[1..].iter().map(|element| String::from(element)).collect::<Vec<String>>();
+    let args = args().collect::<Vec<String>>();
+    if args.len() == 1 {
+        exit(1)
+    }
+    let mut args = args[1..].iter().map(|element| String::from(element)).collect::<Vec<String>>();
     if args.len() != 0 {
         args.remove(0);
     }
@@ -53,3 +57,18 @@ pub fn execute_command(command: CommandExecute) -> CommandResult {
         _ => panic!("How is this possible")
     }
 }
+
+pub async fn openai_results(prompt: String) -> String {
+    String::new()
+}
+// pub async fn chatgpt_results(prompt: String) -> String {
+//     let token = std::env::var("CHATGPT_COOKIE").unwrap();
+//     let mut client = ChatGPT::new(token).expect("Token in wrong");
+//     client.refresh_token().await.expect("could not refersh");
+//     // create a new empty conversation
+//     // let mut conversation = client.new_conversation();
+//     let response = client.send_message(&prompt[..]).await;
+//     let send_response = response.expect("Error receiving response");
+//     // expect("Error receiving response");
+//     send_response
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use code_errors;
+extern crate dotenv;
 
-fn main() {
+use dotenv::dotenv;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
     let command = code_errors::get_command();
     let result = code_errors::execute_command(command);
     print!("Result: Code: {}, error/output:\n{}", result.exit_code, result.error);
+    let prompt = String::from("Provide suggestion for this error with suitable links:\n") + &result.error;
+    // let response = code_errors::chatgpt_results(prompt).await;
+    // print!("Solution is:\n {}", response);
 }
